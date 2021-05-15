fcd67af4759a87adb4079783efff1dccf449de8e
Marko Peng <marko958m@gmail.com>
2021-05-16 02:17:48 +0800

    word transform

--- cmd/word.go
+++ cmd/word.go
@@ -1,12 +1,56 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+	"strings"
+
+	"github.com/Markogoodman/markocommander/internal/word"
+	"github.com/spf13/cobra"
+)
+
+const (
+	ModeUpper = iota + 1
+	ModeLower
+	ModeUnderscoreToUpperCamelCase
+	ModeUnderscoreToLowerCamelCase
+	ModeCamelCaseToUnderscore
+)
+
+var desc = strings.Join([]string{
+	"1: To Upper",
+	"2: To Lower",
+	"3: Underscore to upper camel case",
+	"4: Underscore to lower camel case",
+	"5: Camel case to underscore",
+}, "\n")
 
 var wordCmd = &cobra.Command{
 	Use:   "word",
 	Short: "word format transformer",
-	Long:  "Support different types of transformation",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Long:  desc,
+	Run: func(cmd *cobra.Command, args []string) {
+		var content string
+		switch mode {
+		case ModeUpper:
+			content = word.ToUpper(str)
+		case ModeLower:
+			content = word.ToLower(str)
+		case ModeUnderscoreToUpperCamelCase:
+			content = word.UnderscoreToUpperCamelCase(str)
+		case ModeUnderscoreToLowerCamelCase:
+			content = word.UnderscoreToLowerCamelCase(str)
+		case ModeCamelCaseToUnderscore:
+			content = word.CamelCaseToUnderscore(str)
+		default:
+			log.Fatal("Unsupported mode")
+		}
+		log.Printf("Output: %s", content)
+	},
 }
+var str string
+var mode int8
 
-func init() {}
+func init() {
+	wordCmd.Flags().StringVarP(&str, "str", "s", "", "Input string")
+	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "Input mode")
+}
