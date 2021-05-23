7f57fa5b548633c7c400a7cf5b58fa80b893f6ff
markopeng <markopeng@rayark.com>
2021-05-24 00:05:16 +0800

    generate

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -1,5 +1,13 @@
 package sql2struct
 
+import (
+	"fmt"
+	"html/template"
+	"os"
+
+	"github.com/Markogoodman/markocommander/internal/word"
+)
+
 type StructTemplate struct {
 	structTpl string
 }
@@ -18,4 +26,34 @@ type StructTemplateDB struct {
 
 func NewStructTemplate() *StructTemplate {
 	return &StructTemplate{structTpl: structTpl}
+}
+
+func (t *StructTemplate) AssemblyColumns(tbColumns []*TableColumn) []*StructColumn {
+	tplColumns := make([]*StructColumn, 0, len(tbColumns))
+	for _, column := range tbColumns {
+		tag := fmt.Sprintf("`"+"json:"+"\"%s\""+"`", column.ColumnName)
+		tplColumns = append(tplColumns, &StructColumn{
+			Name:    column.ColumnName,
+			Type:    DBTypeToStructType[column.ColumnType],
+			Tag:     tag,
+			Comment: column.ColumnComment,
+		})
+	}
+	return tplColumns
+}
+
+func (t *StructTemplate) Generate(tableName string, tplColumns []*StructColumn) error {
+	tpl := template.Must(template.New("sql2struct").Funcs(template.FuncMap{
+		"ToCamelCase": word.UnderscoreToUpperCamelCase,
+	}).Parse(t.structTpl))
+
+	tplDB := StructTemplateDB{
+		TableName: tableName,
+		Collumns:  tplColumns,
+	}
+	err := tpl.Execute(os.Stdout, tplDB)
+	if err != nil {
+		return err
+	}
+	return nil
 }
